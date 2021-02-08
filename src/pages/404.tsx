49e760f617e520f3ff2d0fb4e6b82e4d78c43f84
igorssc <igorsantoscosta@gmail.com>
2021-02-08 19:50:16 -0300

    Correction

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import { Title } from '../../styles/Home';
+import { Title } from '../../styles/FourHundredAndFour/FourHundredAndFour';
 
 export default function FourOhFour() {
   return <Title>Erro!!</Title>;
---
+++ styles/FourHundredAndFour/FourHundredAndFour.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 1.2rem;
+  text-transform: uppercase;
+  color: red;
+`;
